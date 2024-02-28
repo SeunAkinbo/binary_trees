51e8edbf4714564aa4296bf838e8169abf46e77e
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-28 05:38:00 -0800

    Updating Uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of the node
+ * @node: Pointer to the node to find the uncle
+ * Return: Pointer to the uncle, NULL otherwise
+ **/
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent->parent)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+
+	return (NULL);
+}
