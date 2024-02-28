c8585d35e4c49c19a09c5ef30118ba8f231e9d30
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-28 08:36:05 -0800

    Updating Is Complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -10,8 +10,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	binary_tree_t **queue;
 	const binary_tree_t *current;
-	int front = 0, rear = 0;
-	int empty_node = 0;
+	int front = 0, rear = 0, empty_node = 0;
 
 	if (!tree)
 		return (0);
@@ -19,24 +18,20 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	queue = malloc(sizeof(binary_tree_t *) * 1024);
 	if (!queue)
 		return (0);
-
 	queue[rear++] = (binary_tree_t *) tree;
 
 	while (front < rear)
 	{
 		current = queue[front++];
-
 		if (empty_node && (current->left && current->right))
 		{
 			free(queue);
-			return (0);
-		}
+			return (0); }
 
 		if (current->right && !current->left)
 		{
 			free(queue);
-			return (0);
-		}
+			return (0); }
 
 		if (current->left)
 			queue[rear++] = current->left;
@@ -44,9 +39,6 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 			empty_node = 1;
 
 		if (current->right)
-			queue[rear++] = current->right;
-	}
-
+			queue[rear++] = current->right; }
 	free(queue);
-	return (1);
-}
+	return (1); }
