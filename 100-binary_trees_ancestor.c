daa1eede65725eb0afb9283ebc59bb6c3a22794a
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-28 07:03:57 -0800

    Updating LCA

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -8,7 +8,8 @@
  *	   of two nodes, NULL if none
  **/
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+					const binary_tree_t *second)
 {
 	const binary_tree_t *temp;
 
