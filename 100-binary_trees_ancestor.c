6dcf3c8f7e81cbaa2892af0f92fc3ea999c08cc8
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-28 07:01:57 -0800

    Updating LCA

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
+ * Return: Pointer to the lowest common ancestor node
+ *	   of two nodes, NULL if none
+ **/
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	const binary_tree_t *temp;
+
+	if (!first || !second)
+		return (NULL);
+
+	if (first == second || first == second->parent ||
+			second == first->parent)
+		return ((binary_tree_t *) first);
+
+	for (; first != NULL; first = first->parent)
+	{
+		for (temp = second; temp != NULL; temp = temp->parent)
+		{
+			if (first == temp)
+				return ((binary_tree_t *) first);
+		}
+	}
+
+	return (NULL);
+}
