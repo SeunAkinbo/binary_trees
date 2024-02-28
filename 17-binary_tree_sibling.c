5d4936bdf083bb7a6be14d8ddfaa092c14b5aca2
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-02-28 05:23:20 -0800

    Updating Sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: Pointer to the node to find the sibling
+ * Return: The pointer to the sibling node, NULL otherwise
+ **/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	
+	if (node == node->parent->right)
+		return (node->parent->left);
+
+	return (NULL);
+}
